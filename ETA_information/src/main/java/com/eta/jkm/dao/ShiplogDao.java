ce9be38a41f00574aa2a3c12b76196a16ff437ce
danmiee <dan-mi@kakao.com>
2023-01-30 14:33:42 +0900

    [230130] FEAT 기준 시각에 따라 데이터 전달하도록 SQL구문 변경

--- ETA_information/src/main/java/com/eta/jkm/dao/ShiplogDao.java
+++ ETA_information/src/main/java/com/eta/jkm/dao/ShiplogDao.java
@@ -21,20 +21,28 @@ public class ShiplogDao {
 
 	public List<ShiplogVO> getLogs() {
 		String sqlString = String.format(
-				"select shipId, shipName, shipUse, shipLat, shipLon, takeTime, speed, departure, departTime, arrivalPort_arrivalName as arrivalName, arrivalTime, accuracy, max(insertTime)\r\n"
-						+ "from ship, schedules s, shiplog sl, arrivalPort a \r\n"
-						+ "where ship.shipId = s.ship_shipId and s.ship_shipId = sl.schedules_ship_shipId and s.arrivalPort_arrivalName = a.arrivalName\r\n"
-						+ "group by shipId;");
+				"select shipId, shipName, shipUse, shipLat, shipLon, takeTime, speed, departure, departTime, arrivalPort_arrivalName as arrivalName, arrivalTime, accuracy, insertTime\r\n"
+				+ "	from ship, schedules s, shiplog sl, arrivalPort a\r\n"
+				+ "	where TIMEDIFF('2023-01-19 16:30:00', insertTime) > 0\r\n"
+				+ "	and shipId = s.ship_shipId \r\n"
+				+ " and shipId = sl.schedules_ship_shipId \r\n"
+				+ " and s.arrivalPort_arrivalName = a.arrivalName;");
 		List<ShiplogVO> list = jdbcTemplate.query(sqlString, new BeanPropertyRowMapper<ShiplogVO>(ShiplogVO.class));
 		return list;
 	}
 
 	public ShiplogVO getLog(Integer shipId) {
 		String sqlString = String.format(
-				"select shipId, shipName, shipUse, shipLat, shipLon, takeTime, speed, departure, departTime, arrivalPort_arrivalName as arrivalName, arrivalTime, accuracy, max(insertTime)\r\n"
-						+ "from ship, schedules s, shiplog sl, arrivalPort a \r\n"
-						+ "where ship.shipId = s.ship_shipId and shipId = sl.schedules_ship_shipId and s.arrivalPort_arrivalName = a.arrivalName and ship.shipId=%d \r\n"
-						+ "group by shipId;",
+				"select shipId, shipName, shipUse, shipLat, shipLon, takeTime, speed, departure, departTime, arrivalPort_arrivalName as arrivalName, arrivalTime, accuracy, insertTime\r\n"
+				+ "	from ship, schedules s, shiplog sl, arrivalPort a\r\n"
+				+ "	where insertTime = (\r\n"
+				+ "		select max(insertTime) from shiplog\r\n"
+				+ "     where TIMEDIFF('2023-01-19 16:30:00', insertTime) > 0\r\n"
+				+ "    )\r\n"
+				+ "	and shipId = s.ship_shipId \r\n"
+				+ " and shipId = sl.schedules_ship_shipId \r\n"
+				+ " and s.arrivalPort_arrivalName = a.arrivalName \r\n"
+				+ " and shipId = %d;",
 				shipId);
 		ShiplogVO info = jdbcTemplate.queryForObject(sqlString, new BeanPropertyRowMapper<ShiplogVO>(ShiplogVO.class));
 		return info;
@@ -42,7 +50,11 @@ public class ShiplogDao {
 
 	public List<ShiplogVO> getLocations(Integer shipId) {
 		String sqlString = String.format(
-				"select shipId, shipLat, shipLon from ship, shiplog sl where ship.shipId = sl.schedules_ship_shipId and ship.shipId=%d",
+				"select shipId, shipLat, shipLon, shipName \r\n"
+				+ "from ship, shiplog sl \r\n"
+				+ "where ship.shipId = sl.schedules_ship_shipId \r\n"
+				+ "and TIMEDIFF('2023-01-19 16:30:00', insertTime) > 0\r\n"
+				+ "and shipId = %d;",
 				shipId);
 		List<ShiplogVO> list = jdbcTemplate.query(sqlString, new BeanPropertyRowMapper<ShiplogVO>(ShiplogVO.class));
 		return list;
