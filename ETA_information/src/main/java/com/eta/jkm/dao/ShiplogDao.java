f2eed6bbd2c2d8cdefb858af4579b15a8f6a18fa
danmiee <dan-mi@kakao.com>
2023-01-30 15:24:00 +0900

    [230130] FIX getLogs 메소드가 shipId별 최근값만 가져오도록 SQL구문 수정

--- ETA_information/src/main/java/com/eta/jkm/dao/ShiplogDao.java
+++ ETA_information/src/main/java/com/eta/jkm/dao/ShiplogDao.java
@@ -21,12 +21,13 @@ public class ShiplogDao {
 
 	public List<ShiplogVO> getLogs() {
 		String sqlString = String.format(
-				"select shipId, shipName, shipUse, shipLat, shipLon, takeTime, speed, departure, departTime, arrivalPort_arrivalName as arrivalName, arrivalTime, accuracy, insertTime\r\n"
+				"select shipId, shipName, shipUse, shipLat, shipLon, takeTime, speed, departure, departTime, arrivalPort_arrivalName as arrivalName, arrivalTime, accuracy, max(insertTime)\r\n"
 				+ "	from ship, schedules s, shiplog sl, arrivalPort a\r\n"
 				+ "	where TIMEDIFF('2023-01-19 16:30:00', insertTime) > 0\r\n"
 				+ "	and shipId = s.ship_shipId \r\n"
 				+ " and shipId = sl.schedules_ship_shipId \r\n"
-				+ " and s.arrivalPort_arrivalName = a.arrivalName;");
+				+ " and s.arrivalPort_arrivalName = a.arrivalName \r\n"
+				+ " group by shipId;");
 		List<ShiplogVO> list = jdbcTemplate.query(sqlString, new BeanPropertyRowMapper<ShiplogVO>(ShiplogVO.class));
 		return list;
 	}
