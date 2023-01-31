a29de31c944ee5766984f8688199323ebffcf167
danmiee <dan-mi@kakao.com>
2023-01-31 14:26:17 +0900

    230131 COMMENT 메소드 내 주석 추가

--- ETA_information/src/main/java/com/eta/jkm/dao/ShiplogDao.java
+++ ETA_information/src/main/java/com/eta/jkm/dao/ShiplogDao.java
@@ -21,42 +21,55 @@ public class ShiplogDao {
 
 	public List<ShiplogVO> getLogs(Integer timeGroup) {
 		String sqlString = String.format(
-				"select shipId, shipName, shipUse, shipLat, shipLon, takeTime, speed, departure, departTime, arrivalPort_arrivalName as arrivalName, arrivalTime, accuracy, insertTime\r\n"
-				+ "from ship, schedules s, shiplog sl, arrivalPort a\r\n"
-				+ "where timeGroup = %d\r\n"
-				+ "and shipId = s.ship_shipId\r\n"
-				+ "and shipId = sl.schedules_ship_shipId\r\n"
-				+ "and s.arrivalPort_arrivalName = a.arrivalName\r\n"
-				+ "and status = 0\r\n"
-				+ "group by shipId;",
-				timeGroup);
+			// 필요한 정보 가져오기
+			"select shipId, shipName, shipUse, shipLat, shipLon, takeTime, speed, departure, departTime, arrivalPort_arrivalName as arrivalName, arrivalTime, accuracy, insertTime\r\n"
+			// 가져올 테이블
+			+ "from ship, schedules s, shiplog sl, arrivalPort a\r\n"
+			// 조건 설정
+			// 데이터 입력 시각 - emulator 활용으로 동일한 시각에 보고한다는 전제 하에 넘버링 사용
+			// 실제 서비스를 진행하고자 할 때에는 관련 코드 별도 작성 필요
+			+ "where timeGroup = %d\r\n"
+			+ "and shipId = s.ship_shipId\r\n"						// ship, schedule 테이블 연결
+			+ "and shipId = sl.schedules_ship_shipId\r\n"			// ship, shiplog 테이블 연결
+			+ "and s.arrivalPort_arrivalName = a.arrivalName\r\n"	// ship, arrivalport 테이블 연결
+			+ "and status = 0\r\n"									// 운항중인 정보만 가져오기(0:운항중 / 1:운항종료)
+			+ "group by shipId;",									// shipId(mmsi) 기준으로 그룹화
+			timeGroup);
 		List<ShiplogVO> list = jdbcTemplate.query(sqlString, new BeanPropertyRowMapper<ShiplogVO>(ShiplogVO.class));
 		return list;
 	}
 
 	public ShiplogVO getLog(Integer timeGroup, Integer shipId) {
 		String sqlString = String.format(
-				"select shipId, shipName, shipUse, shipLat, shipLon, takeTime, speed, departure, departTime, arrivalPort_arrivalName as arrivalName, arrivalTime, accuracy, insertTime\r\n"
-				+ "from ship, schedules s, shiplog sl, arrivalPort a\r\n"
-				+ "where timeGroup = %d \r\n"
-				+ "and shipId = s.ship_shipId \r\n"
-				+ "and shipId = sl.schedules_ship_shipId \r\n"
-				+ "and s.arrivalPort_arrivalName = a.arrivalName \r\n"
-				+ "and status = 0\r\n"
-				+ "and shipId = %d;",
-				timeGroup, shipId);
+			// 필요한 정보 가져오기
+			"select shipId, shipName, shipUse, shipLat, shipLon, takeTime, speed, departure, departTime, arrivalPort_arrivalName as arrivalName, arrivalTime, accuracy, insertTime\r\n"
+			// 가져올 테이블
+			+ "from ship, schedules s, shiplog sl, arrivalPort a\r\n"
+			// 조건 설정
+			// 데이터 입력 시각 - emulator 활용으로 동일한 시각에 보고한다는 전제 하에 넘버링 사용
+			// 실제 서비스를 진행하고자 할 때에는 관련 코드 별도 작성 필요
+			+ "where timeGroup = %d \r\n"
+			+ "and shipId = s.ship_shipId \r\n"						// ship, schedule 테이블 연결
+			+ "and shipId = sl.schedules_ship_shipId \r\n"			// ship, shiplog 테이블 연결
+			+ "and s.arrivalPort_arrivalName = a.arrivalName \r\n"	// ship, arrivalport 테이블 연결
+			+ "and status = 0\r\n"									// 운항중인 정보만 가져오기(0:운항중 / 1:운항종료)
+			+ "and shipId = %d;",									// 조회할 shipId(mmsi)
+			timeGroup, shipId);
 		ShiplogVO info = jdbcTemplate.queryForObject(sqlString, new BeanPropertyRowMapper<ShiplogVO>(ShiplogVO.class));
 		return info;
 	}
 
 	public List<ShiplogVO> getLocations(Integer timeGroup, Integer shipId) {
 		String sqlString = String.format(
+			// 필요한 정보 가져오기
 			"select shipId, shipLat, shipLon, insertTime \r\n"
+			// 가져올 테이블
 			+ "from ship, shiplog sl \r\n"
-			+ "where shipId = sl.schedules_ship_shipId \r\n"
-			+ "and timeGroup <= %d \r\n"
-			+ "and status = 0\r\n"
-			+ "and shipId = %d;",
+			// 조건 설정
+			+ "where shipId = sl.schedules_ship_shipId \r\n"	// ship, shiplog 테이블 연결
+			+ "and timeGroup <= %d \r\n"						// 데이터 입력 시각
+			+ "and status = 0\r\n"								// 운항여부
+			+ "and shipId = %d;",								// 조회할 shipId(mmsi)
 			timeGroup, shipId);
 		List<ShiplogVO> list = jdbcTemplate.query(sqlString, new BeanPropertyRowMapper<ShiplogVO>(ShiplogVO.class));
 		return list;
