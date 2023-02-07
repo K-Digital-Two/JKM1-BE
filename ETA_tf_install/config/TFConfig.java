c73abd564ac50b6f4d1bb12e3d32898b5bd14c28
변혁 (Byeon Hyeok) <111625691+uiiunm@users.noreply.github.com>
2023-02-07 11:37:41 +0900

    [230201] FEAT tensorflow 설치 및 예제 출력

---
+++ ETA_tf_install/config/TFConfig.java
@@ -0,0 +1,14 @@
+package com.eta.jkm.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TFConfig {
+
+	@Bean
+	public Object TFInit() {
+//		TensorFlow.
+		return null;
+	}
+}
