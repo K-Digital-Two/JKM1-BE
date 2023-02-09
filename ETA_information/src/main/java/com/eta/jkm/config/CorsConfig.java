31721d0ab4c2ac38e764c3889d88baa2081a4d46
danmiee <dan-mi@kakao.com>
2023-02-09 18:08:00 +0900

    [230209] FIX cors error 해결

--- ETA_information/src/main/java/com/eta/jkm/config/CorsConfig.java
+++ ETA_information/src/main/java/com/eta/jkm/config/CorsConfig.java
@@ -10,12 +10,6 @@ public class CorsConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:3000", 
-					"http://10.125.121.170:3000", // 이창현
-					"http://10.125.121.186:3000", // 이정민
-					"http://10.125.121.204:3000", // 금민경 
-					"http://10.125.121.173:3000", // 최정인
-					"http://10.125.121.203:3000" // 변혁
-					);
+			.allowedOrigins("http://localhost:3000", "http://10.125.121.170:3000");
 	}
 }
